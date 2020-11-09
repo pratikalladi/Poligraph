b57513461e7cd486cb9ed43749dc7a8fc5c1b5a6
Pratik Alladi <pratikalladi@gmail.com>
2020-11-09 00:57:38 -0500

    added week 7 work, API keys and twitter4j integration

--- src/main/java/org/pratik/Poligraph.java
+++ src/main/java/org/pratik/Poligraph.java
@@ -1,42 +1,65 @@
 package org.pratik;
 
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 import twitter4j.conf.ConfigurationBuilder;
-import twitter4j.Query;
-import twitter4j.QueryResult;
-import twitter4j.Status;
+
 import java.util.ArrayList;
-import java.time.ZonedDateTime;
 
 public class Poligraph {
-    Twitter twitter = TwitterFactory.getSingleton();
 
+    Twitter twitter;
 
-    public ArrayList<String> getTweetsInRange(ZonedDateTime start, ZonedDateTime end)
+    public Poligraph(String cKey, String cKeySecret, String aToken, String aTokenSecret)
     {
-      twitter.search()
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+        cb.setDebugEnabled(true)
+                .setOAuthConsumerKey(cKey)
+                .setOAuthConsumerSecret(cKeySecret)
+                .setOAuthAccessToken(aToken)
+                .setOAuthAccessTokenSecret(aTokenSecret);
+        TwitterFactory tf = new TwitterFactory(cb.build());
+        twitter = tf.getInstance();
     }
 
-    public static void main(String[] args) throws TwitterException
-     {
+
+    // searchWord can be user id, place etc
+    // date should be in the format yyyy-MM-dd
+    public ArrayList<Status> getTweetsInRange(String startDate, String endDate, String searchWord, int count) throws TwitterException {
+        Query query = new Query(searchWord);
+        query.setCount(count);
+        query.setSince(startDate);
+        query.setUntil(endDate);
+        return twitterQuery(query);
+    }
+
+    public ArrayList<Status> twitterQuery(Query query) throws TwitterException {
+        QueryResult result = twitter.search(query);
+        return new ArrayList<>(result.getTweets());
+    }
+
+    public static void main(String[] args) throws TwitterException {
+
+        String cKey = "FZR30B3lQbMVoNEZBspeoJKPH";
+        String cKeySecret = "So12YRUjpr9qM9wfR921toN1yAmdvlJ2LkWIVt0bNuJF4a89Q6";
+        String aToken = "1325292306413576194-URa206WZicJkTcCN92DaB2LDx78Pjr";
+        String aTokenSecret = "1MLcfJUZxumquQtIDpbPsDWU8jdPr6OFbMurFcN9Mldzk";
 
 
-         ConfigurationBuilder cb = new ConfigurationBuilder();
-         cb.setDebugEnabled(true
-                 .setOAuthConsumerKey("consumer key")
-                 .setOAuthConsumerSecret("consumer secret")
-                 .setOAuthAccessToken("access token")
-                 .setOAuthAccessTokenSecret("access token secret")
-         TwitterFactory tf = new TwitterFactory(cb.build());
-         Twitter twitter = tf.getInstance();
-     Query query = new Query("#google");
+        Query query = new Query("#iPhone");
+        query.setCount(100);
+        Poligraph p = new Poligraph(cKey, cKeySecret, aToken, aTokenSecret);
 
-         QueryResult result = twitter.search(query);
+        ArrayList<Status> result = p.getTweetsInRange("2020-11-01", "2020-11-07", "#Biden", 100);
 
-         for (Status status: result.getTweets())
-           System.out.println("Status@\t" + status.getUser().getScreenName() + "\t:\t" + status.getText());
 
-        }
+
+        for (Status status : result)
+            System.out.println("Status@\t" + status.getUser().getScreenName() + "\t:\t" + status.getText());
+
+    }
 }
