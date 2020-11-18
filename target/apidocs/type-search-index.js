c71f5d28c2d953aca1e035cfbfc3e543f8c80bbb
Pratik Alladi <pratikalladi@gmail.com>
2020-11-17 23:04:56 -0500

    deleted excess files - gitignore

--- target/apidocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.github.pratikalladi","l":"Poligraph"},{"p":"com.github.pratikalladi","l":"Result"},{"p":"com.github.pratikalladi","l":"TwitterSentimentAnalysis"}]
